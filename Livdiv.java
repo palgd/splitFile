2a2bd2533891959a21bcbcf667db3482f90cb876
palgd <mememeron21@gmail.com>
2013-06-19 21:14:30 +0900

    hoge

--- Livdiv.java
+++ Livdiv.java
@@ -11,14 +11,16 @@ import java.io.PrintWriter;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Livdiv {
 	private static final String SPLIT_OPTION = "-b 1MB";
-	private ArrayList<String> div_parts_list = new ArrayList<String>();
 
+	//TODO
 	// div_convert file
 	public void div_convert(String src){
 		String dst = div_name(src);
@@ -28,7 +30,6 @@ public class Livdiv {
 			div_create(dst);
 		}
 
-		//TODO
 	}
 
 	// div_name file
@@ -56,66 +57,58 @@ public class Livdiv {
 		File file = new File(dst);
 		File linked = new File(dst + "/linked");
 		File sha1 = new File(dst + "/sha1");
+		File lsl = new File(dst+"ls-l");
 
 		if(! file.isDirectory()){
 			file.mkdirs();
 			div_create_ls_l(div_name(dst));
-			try {
-				linked.createNewFile();
-				sha1.createNewFile();
-			} catch (IOException e) {
-				System.out.println(e);
+			if(! linked.exists() && ! sha1.exists()){
+				try {
+					linked.createNewFile();
+					sha1.createNewFile();
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+			}else{
+				linked.setLastModified(lsl.lastModified());
+				sha1.setLastModified(lsl.lastModified());
 			}
 		}
 	}
 
 	// div_create_ls_l
 	public void div_create_ls_l(String dst){
-		//touch
 		File file = new File(dst +"/" + dst);
+		File lsl = new File(dst + "/ls-l");
+		Calendar cal = Calendar.getInstance();
+		String[] ls_l = lsl(file);
 
-		try {
-			file.createNewFile();
-		} catch (IOException e) {
-			System.out.println(e);
+		//touch
+		if(! file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}else{
+			file.setLastModified(cal.getTimeInMillis());
 		}
 
 		// ls -l >ls-l
-		File lsl = new File(dst + "/ls-l");
-
 		try {
 			lsl.createNewFile();
 		} catch (IOException e) {
 			System.out.println(e);
 		}
-
+		
 		try {
-			FileWriter filewriter = new FileWriter(lsl);
-
-			if(file.canRead()){
-				filewriter.write("r");
-			}else{
-				filewriter.write("-");
+			FileWriter fw = new FileWriter(lsl);
+			for(int i = 0; i < ls_l.length; i++){
+				fw.write(ls_l[i]+" ");
 			}
-
-			if(file.canWrite()){
-				filewriter.write("w");
-			}else{
-				filewriter.write("-");
-			}
-
-			if(file.canExecute()){
-				filewriter.write("x ");
-			}else{
-				filewriter.write("- ");
-			}
-			
-			String size = String.valueOf(file.length());
-			filewriter.write(size+" ");
-
-			filewriter.close();
+			fw.close();
 		} catch (IOException e) {
-			System.out.println(e);;
+			System.out.println(e);
 		}
 
 		file.delete();
@@ -123,7 +116,7 @@ public class Livdiv {
 
 	// TODO
 	// div_write file.dir
-	public void div_write(String s){
+	public void div_write(String s, File src){
 		String dst = follow_link(s);
 		File file = new File(dst);
 
@@ -132,6 +125,7 @@ public class Livdiv {
 		}else{
 			div_trunc(dst);
 		}
+		
 	}
 
 	// follow_link
@@ -154,11 +148,10 @@ public class Livdiv {
 		return src;
 	}
 
-	// TODO
 	// div_trunk
 	public void div_trunc(String s){
 		String dst = follow_link(s);
-		div_parts(dst);
+		ArrayList<String> div_parts_list = div_parts(dst);
 
 		for(int i = 0; i < div_parts_list.size(); i++){
 			File file = new File(div_parts_list.get(i));
@@ -172,11 +165,12 @@ public class Livdiv {
 	}
 
 	// div_part
-	public void div_parts(String s){
+	public ArrayList<String> div_parts(String s){
 		String src = "", dst = follow_link(s);
 		File file = new File(dst);
 		String[] fileName = file.list();
 		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> div_parts_list = new ArrayList<String>();
 		Pattern pm = Pattern.compile("^M");
 		Pattern pref = Pattern.compile("$ref");
 
@@ -197,14 +191,10 @@ public class Livdiv {
 			if(mref.find()){
 				String d = file.getParent();
 
-				// cat
 				try{
 					FileReader f = new FileReader(d + "/refer");
 					BufferedReader b = new BufferedReader(f);
-					String cat;
-					while(((cat = b.readLine())!=null)){
-						src = cat;
-					}
+					src = b.readLine();
 					b.close();
 				}catch(FileNotFoundException e){
 					System.out.println(e);
@@ -220,21 +210,21 @@ public class Livdiv {
 				div_parts_list.add(list.get(i));
 			}
 		}
+		return div_parts_list;
 	}
 
-	// TODO
 	// div_change_size
 	public void div_change_size(String s){
 		String dst = follow_link(s);
 		int size = div_size(dst);
-		div_replace_ls_l(dst,4,size);
+		div_replace_ls_l(dst,1,size);
 	}
 
 	// div_size and div_count
 	public int div_size(String s){
 		int size = 0;
 
-		div_parts(s);
+		ArrayList<String> div_parts_list = div_parts(s);
 
 		for(int i = 0; i < div_parts_list.size();i++){
 			File file = new File(div_parts_list.get(i));
@@ -244,35 +234,134 @@ public class Livdiv {
 		return size;
 	}
 
-	//TODO
 	// div_replace_ls_l
 	public void div_replace_ls_l(String dst, int n, int val){
 		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
 		String vmName = bean.getName();
 		long pid = Long.valueOf(vmName.split("@")[0]);
 		String tmp = dst+"div_replace_ls_l."+pid;
-
+		String[] sa = div_substr_ls_l(dst,n,val);
 		File file = new File(tmp);
+		File lsl = new File(dst+"/ls-l");
+
+		if(!file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
 		try {
 			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
-			pw.write(div_substr_ls_l(dst,n,val));
+			for (int i = 0; i < sa.length; i++){
+				pw.write(sa[i] + " ");
+			}
+			pw.close();
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 
+		file.setLastModified(lsl.lastModified());
+		file.renameTo(lsl);
+
 	}
 
-	//TODO
 	// div_substr_ls_l
-	public String div_substr_ls_l(String src, int n, int val){
-		
-		return "";
+	public String[] div_substr_ls_l(String src, int n, int val){
+		String[] sa = null;
+		try {
+			BufferedReader b = new BufferedReader(new FileReader(src+"ls-l"));
+			sa = b.readLine().split(" ");
+			sa[n] = String.valueOf(val);
+			b.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		return sa;
 	}
 
-	// TODO
 	// div_change_mtime_now
 	public void div_change_mtime_now(String s){
+		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+		String vmName = bean.getName();
+		long pid = Long.valueOf(vmName.split("@")[0]);
+		String dst = follow_link(s);
+		String now = dst + "now."+ pid;
+		File file = new File(now);
+		Calendar cal = Calendar.getInstance();
 
+		if(! file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}else{
+			file.setLastModified(cal.getTimeInMillis());
+		}
+
+		div_change_mtime(dst,now);
+		file.delete();
 	}
 
+	// div_change_mtime
+	public void div_change_mtime(String s,String last){
+		String dst = follow_link(s);
+		File file = new File(last);
+		String[] ls_l = lsl(file);
+		
+		div_replace_ls_l(dst, 2, Integer.valueOf(ls_l[2]));
+		div_replace_ls_l(dst, 3, Integer.valueOf(ls_l[3]));
+	}
+
+	/*
+	public int[] getLastModified(File file){
+		Calendar cal = Calendar.getInstance();
+		Date date = new Date(file.lastModified());
+		cal.setTime(date);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DATE);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int minute = cal.get(Calendar.MINUTE);
+		int[] lastModified ={month,day,hour,minute};
+		return lastModified;
+	}*/
+
+	// ls -l
+	public String[] lsl(File file){
+		String[] ls_l = new String[5];
+		Date date = new Date(file.lastModified());
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DATE);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int minute = cal.get(Calendar.MINUTE);
+
+		if(file.canRead()){
+			ls_l[0] = "r";
+		}else{
+			ls_l[0] = "-";
+		}
+		if(file.canWrite()){
+			ls_l [0] += "w";
+		}else{
+			ls_l [0] += "-";
+		}
+		if(file.canExecute()){
+			ls_l [0] += "x";
+		}else{
+			ls_l [0] += "-";
+		}
+		ls_l[1] = String.valueOf(file.length());
+		ls_l[2] = String.valueOf(month);
+		ls_l[3] = String.valueOf(day);
+		ls_l[4] = String.valueOf(hour) + ":" + String.valueOf(minute);
+		ls_l[5] = file.getName();
+		
+		return ls_l;
+	}
 }
--- Test.java
+++ Test.java
@@ -1,23 +1,51 @@
 package splitFile;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 
-
 public class Test {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
 		File file = new File(args[0]);
-		File fa = new File(args[1]);
-		FileWriter f = new FileWriter(file);
-		long lastModifytime = fa.lastModified();
-		Date date = new Date(lastModifytime);
+		String[] s = lsl(file);
+		for(int i = 0; i < s.length; i++){
+			System.out.print(s[i] +" ");
+		}
+		System.out.println();
+	}
+	
+	public static String[] lsl(File file){
+		String[] ls_l = new String[6];
+		Date date = new Date(file.lastModified());
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
-		int month = cal.MONTH;
-		f.write("aa "+ file.length() +" " + ((cal.MONTH) +1) +" " + (cal.DATE) +" "+ file.getName());
-		f.close();
+
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DATE);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int minute = cal.get(Calendar.MINUTE);
+
+		if(file.canRead()){
+			ls_l[0] = "r";
+		}else{
+			ls_l[0] = "-";
+		}
+		if(file.canWrite()){
+			ls_l [0] += "w";
+		}else{
+			ls_l [0] += "-";
+		}
+		if(file.canExecute()){
+			ls_l [0] += "x";
+		}else{
+			ls_l [0] += "-";
+		}
+		ls_l[1] = String.valueOf(file.length());
+		ls_l[2] = String.valueOf(month);
+		ls_l[3] = String.valueOf(day);
+		ls_l[4] = String.valueOf(hour) + ":" + String.valueOf(minute);
+		ls_l[5] = file.getName();
+		
+		return ls_l;
 	}
 }
