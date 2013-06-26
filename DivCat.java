04ba6dfd328e31dbfd51e96ed487e70a26988ac2
palgd <mememeron21@gmail.com>
2013-06-26 23:32:32 +0900

    hoge

--- DivCat.java
+++ DivCat.java
@@ -1,11 +1,39 @@
 package splitFile;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class DivCat {
 	public static void main(String[] args) {
+		String arg = "", options = "";
+		int ch = 0;
 		Livdiv livdiv = new Livdiv();
-		
+		Pattern p = Pattern.compile("-.");
+
 		for(int i = 0; i < args.length; i++){
-			
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(new File(args[i])));
+				if(args[i].equals("--help") || args[i].equals("--version")){
+					while((ch = br.read()) != -1){
+						System.out.println((char)(ch));
+					}
+					br.close();
+					return;
+				}else if(args[i].equals("-")){
+					
+				}else if(true){
+					
+				}
+				br.close();
+			}catch (FileNotFoundException e) {
+				System.out.println(e);
+			} catch (IOException e) {
+				System.out.println(e);
+			}
 		}
 	}
 }
--- Test.java
+++
@@ -1,8 +0,0 @@
-package splitFile;
-
-public class Test {
-	public static void main(String[] args) {
-		Livdiv l = new Livdiv();
-		l.divRestore(args[0]);
-	}
-}
